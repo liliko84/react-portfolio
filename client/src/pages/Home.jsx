517359ce7788acd07832adeebaf3c8093e0326c2
lili gugushvili <lilikog@yahoo.com>
2020-04-25 12:16:34 -0400

    all files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap'
+import { Jumbotron} from 'react-bootstrap'
 
 export default () => {
   return (<>
     <Jumbotron className="mt-3">
 
-      <h1>About Me</h1>
+      
+        <p> I have 7 years of work experience back in my Country:
+              My first Job was as an Operator in Internet Provider Company "Georgia Online", which was very hard at the
+              beggining because had no idea about computers, but then got used to it and learned lots of interesting
+              things.
+              Then I was manager in Mobile Content Provider "Geoline", which was also very fun and interesting.
+              The last Company that I was working was Aviation Fuel Supply Company "Georgian Petroleum" where I was
+              Assistant
+              of the General director.</p>
+      
       
 
     </Jumbotron>
