158c93231fd9b1ab25237377fbc2e0d315bc973c
lili gugushvili <lilikog@yahoo.com>
2020-05-03 16:30:31 -0400

    all files

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -1,5 +1,58 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import './portfolio.css';
+import bootstrap from './assets/img/bootstrap.png';
+import burger from './assets/img/burger.png';
+import planner from './assets/img/planner.png';
+import fitness from './assets/img/fitness.png';
+import note from './assets/img/note.png';
+import password from './assets/img/password.png';
+import travel from './assets/img/travel.png';
+import directory from './assets/img/directory.png';
+import weather from './assets/img/weather.png';
+
 
 export default () => {
-  return (<>Portfolio</>)
-}
+  return (
+  <>
+  <Jumbotron className="bg-info text-light">
+           <Container className="text-center">
+               <h1 className='display-3'>Portfolio</h1>
+           </Container>
+       </Jumbotron>
+       <Container>
+          <a className="blue" href=" "> <img className='password' src={password}/></a>
+          <h2>Password Generator</h2>
+          </Container>
+
+          <Container>
+          <a className="blue" href=""><img className='burger' src={burger}/></a>           
+          <h2>Burger App</h2>
+          </Container>
+
+          <Container>
+          <a className="blue" href=""> <img className='planner' src={planner}/></a>  
+          <h2>Day Planner</h2>
+          </Container>
+
+          <Container>
+          <a className="blue" href=""> <img className='fitness' src={fitness}/></a>  
+          <h2>Fitness Tracker</h2>
+          </Container>
+
+          <Container>
+          <a className="blue" href=""><img className='note' src={note}/></a> 
+          <h2>Note Taker</h2>
+          </Container>
+
+          <Container>
+          <a className="blue" href=" "><img className='password' src={password}/></a>
+          <h2>Password Generator</h2>
+          </Container>
+         
+  </>
+  )
+  }
+
+  export default Portfolio;
